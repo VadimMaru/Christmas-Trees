6b12694c06a9f39118e656efc79cbf6ffb98aac6
Vadim Gridin <v.gridin@list.ru>
2022-12-30 20:27:03 +0300

    inin

---
+++ script/burger.js
@@ -0,0 +1,7 @@
+const burger = document.querySelector('.burger');
+const navigation = document.querySelector('.navigation');
+
+burger.addEventListener('click' , () => {
+    burger.classList.toggle('burger-active');
+    navigation.classList.toggle('navigation-active');
+});
